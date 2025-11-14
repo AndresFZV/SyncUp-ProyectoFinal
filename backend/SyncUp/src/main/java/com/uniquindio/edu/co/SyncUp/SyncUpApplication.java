87b7274d4e303322182c1e1f9ceb9ba509804aff
AndresFZV <andresf.zambranov@uqvirtual.edu.co>
2025-11-14 12:23:31 -0500

    Actualización de la aplicación
    Se actualizó la app con el EnableSync

--- backend/SyncUp/src/main/java/com/uniquindio/edu/co/SyncUp/SyncUpApplication.java
+++ backend/SyncUp/src/main/java/com/uniquindio/edu/co/SyncUp/SyncUpApplication.java
@@ -2,8 +2,10 @@ package com.uniquindio.edu.co.SyncUp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class SyncUpApplication {
 
 	public static void main(String[] args) {
