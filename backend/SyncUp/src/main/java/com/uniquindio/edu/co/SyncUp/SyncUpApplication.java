875b6aafc3e23fd15f0eb5fc0480acf6ad38553e
AndresFZV <andresf.zambranov@uqvirtual.edu.co>
2025-11-17 02:51:07 -0500

    Documentación App

--- backend/SyncUp/src/main/java/com/uniquindio/edu/co/SyncUp/SyncUpApplication.java
+++ backend/SyncUp/src/main/java/com/uniquindio/edu/co/SyncUp/SyncUpApplication.java
@@ -4,10 +4,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+/**
+ * Clase principal de la aplicación SyncUp - Sistema de Streaming Musical.
+ * Inicia la aplicación Spring Boot y configura la funcionalidad asíncrona.
+ *
+ * @author SyncUp Team
+ * @version 1.0
+ */
 @SpringBootApplication
 @EnableAsync
 public class SyncUpApplication {
 
+	/**
+	 * Método principal que inicia la aplicación Spring Boot.
+	 *
+	 * @param args Argumentos de línea de comandos
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(SyncUpApplication.class, args);
 	}
